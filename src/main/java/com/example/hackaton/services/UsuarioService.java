684d13e431bb5c35d794bcf1df3c4c15fe20fcf8
Christian Santiago Vera Rojas <104735286+SantiagoVera9812@users.noreply.github.com>
2024-04-27 19:55:17 -0500

    se ha terminado el registro y inicio de sesion de un usuario

---
+++ src/main/java/com/example/hackaton/services/UsuarioService.java
@@ -0,0 +1,41 @@
+package com.example.hackaton.services;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.hackaton.DTO.UsuarioDTO;
+import com.example.hackaton.modelo.Usuario;
+import com.example.hackaton.repositories.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    public Usuario insertar(UsuarioDTO usuario){
+
+        Usuario nuevoUsuario = new Usuario();
+
+        nuevoUsuario.setNombre(usuario.getNombre());
+        nuevoUsuario.setApellido(usuario.getApellido());
+        nuevoUsuario.setCiudad(usuario.getCiudad());
+        nuevoUsuario.setCelular(usuario.getCelular());
+        nuevoUsuario.setContrasena(usuario.getContrasena());
+        nuevoUsuario.setCorreo(usuario.getCorreo());
+
+        return usuarioRepository.save(nuevoUsuario);
+    }
+
+    public boolean verificarUsuario(String correo, String password) {
+        
+        Usuario usuario = usuarioRepository.findByCorreoAndContrasena(correo, password);
+        return usuario != null;
+
+    }
+    
+}
